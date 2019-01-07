b122dddfb764bb53abe895b54a18f2953201fcec
yuckyh <yuckychong@gmail.com>
2019-01-07 20:34:45 +0700

    Create gallery support for announcement

--- resources/views/layout/announcement/partial/content.blade.php
+++ resources/views/layout/announcement/partial/content.blade.php
@@ -22,7 +22,9 @@
 </div>
 @endif
 @if(str_contains($type->files, '.jpg') || str_contains($type->files, '.jpeg') || str_contains($type->files, '.gif')  || str_contains($type->files, '.png'))
-<div class="announcement-img">
-    <img src="file/{{ $type->files }}" alt="No Photo" style="height: 15rem; width: 20rem;">
+<div class="announcement-gallery">
+    <img src="file/{{ $type->files }}" alt="No Photo">
+    <img src="file/{{ $type->files }}" alt="No Photo">
+    <img src="file/{{ $type->files }}" alt="No Photo">
 </div>
 @endif
